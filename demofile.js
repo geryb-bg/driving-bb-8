01ebf3063614115cdcd7ef1c69b87d36d5ff2a7a
Gergana Young <gergana@bbd.co.za>
2016-06-27 20:05:38 +0200

    adding file for presentation

---
+++ demofile.js
@@ -0,0 +1,19 @@
+var Cylon = require('cylon');
+
+var datajson = require('./data.json');
+
+var myUUId = datajson.devices.bb8;
+
+Cylon.robot({
+  connections: {
+    bluetooth: { adaptor: 'central', uuid: myUUId, module: 'cylon-ble' }
+  },
+
+  devices: {
+    bb8: { driver: 'bb8', module: 'cylon-sphero-ble' }
+  },
+
+  work: function(my) {
+              
+  }
+}).start();
